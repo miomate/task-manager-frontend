9eb911ae39f052bdfeda94c01afdaf51e4bcf1ab
miomate <mrspec@outlook.com>
2025-02-11 15:58:24 +0100

    building frontend

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>test</h1>
-    <App />
+    <Router>
+      <h1>test</h1>
+      <App />
+    </Router>
   </StrictMode>,
 )
